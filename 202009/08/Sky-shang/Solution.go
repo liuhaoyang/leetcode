9901d01abb874cb37eec485b11b7bbb2753113aa
shang <1006979524@qq.com>
2020-09-08 22:30:32 +0800

    Auto Merge

---
+++ 202009/08/Sky-shang/Solution.go
@@ -0,0 +1,25 @@
+func combine(n int, k int) (ans [][]int) {
+	temp := []int{}
+	var dfs func(int)
+	dfs = func(cur int) {
+		// 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
+		if len(temp) + (n - cur + 1) < k {
+			return
+		}
+		// 记录合法的答案
+		if len(temp) == k {
+			comb := make([]int, k)
+			copy(comb, temp)
+			ans = append(ans, comb)
+			return
+		}
+		// 考虑选择当前位置
+		temp = append(temp, cur)
+		dfs(cur + 1)
+		temp = temp[:len(temp)-1]
+		// 考虑不选择当前位置
+		dfs(cur + 1)
+	}
+	dfs(1)
+	return
+}
