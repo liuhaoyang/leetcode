313de3e1122f64d102ece7eb5f1148b0ddb76342
Sky-Shang <1006979524@qq.com>
2020-09-11 21:17:22 +0800

    911打卡

---
+++ 202009/11/Sky-shang/Solution.go
@@ -0,0 +1,21 @@
+func combinationSum3(k int, n int) (ans [][]int) {
+	var temp []int
+	check := func(mask int) bool {
+		temp = nil
+		sum := 0
+		for i := 0; i < 9; i++ {
+			if 1<<i&mask > 0 {
+				temp = append(temp, i+1)
+				sum += i + 1
+			}
+		}
+		return len(temp) == k && sum == n
+	}
+
+	for mask := 0; mask < 1<<9; mask++ {
+		if check(mask) {
+			ans = append(ans, append([]int(nil), temp...))
+		}
+	}
+	return
+}
