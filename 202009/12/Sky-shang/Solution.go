d440d64f5323843ce65a62d0cdebcae9d6f81dc9
Sky-Shang <1006979524@qq.com>
2020-09-12 22:36:35 +0800

    912打卡

---
+++ 202009/12/Sky-shang/Solution.go
@@ -0,0 +1,26 @@
+type data struct{ sum, count int }
+
+func averageOfLevels(root *TreeNode) []float64 {
+	levelData := []data{}
+	var dfs func(node *TreeNode, level int)
+	dfs = func(node *TreeNode, level int) {
+		if node == nil {
+			return
+		}
+		if level < len(levelData) {
+			levelData[level].sum += node.Val
+			levelData[level].count++
+		} else {
+			levelData = append(levelData, data{node.Val, 1})
+		}
+		dfs(node.Left, level+1)
+		dfs(node.Right, level+1)
+	}
+	dfs(root, 0)
+
+	averages := make([]float64, len(levelData))
+	for i, d := range levelData {
+		averages[i] = float64(d.sum) / float64(d.count)
+	}
+	return averages
+}
