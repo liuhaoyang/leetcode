90cf8aa7b936f9d025504886ddd126e85a9b2be1
shang <1006979524@qq.com>
2020-09-13 22:00:48 +0800

    Auto Merge

---
+++ 202009/13/Sky-shang/Solution.go
@@ -0,0 +1,38 @@
+type pair struct{ x, y int }
+
+var directions = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 上下左右
+
+func exist(board [][]byte, word string) bool {
+	h, w := len(board), len(board[0])
+	vis := make([][]bool, h)
+	for i := range vis {
+		vis[i] = make([]bool, w)
+	}
+	var check func(i, j, k int) bool
+	check = func(i, j, k int) bool {
+		if board[i][j] != word[k] { // 剪枝：当前字符不匹配
+			return false
+		}
+		if k == len(word)-1 { // 单词存在于网格中
+			return true
+		}
+		vis[i][j] = true
+		defer func() { vis[i][j] = false }() // 回溯时还原已访问的单元格
+		for _, dir := range directions {
+			if newI, newJ := i+dir.x, j+dir.y; 0 <= newI && newI < h && 0 <= newJ && newJ < w && !vis[newI][newJ] {
+				if check(newI, newJ, k+1) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+	for i, row := range board {
+		for j := range row {
+			if check(i, j, 0) {
+				return true
+			}
+		}
+	}
+	return false
+}
