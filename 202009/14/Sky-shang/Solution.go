afa375b905fa98e232e2ac0539cdea02d8e51201
Sky-Shang <1006979524@qq.com>
2020-09-14 22:36:38 +0800

    914打卡

---
+++ 202009/14/Sky-shang/Solution.go
@@ -0,0 +1,13 @@
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return
+}
